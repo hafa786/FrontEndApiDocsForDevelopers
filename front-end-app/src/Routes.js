834fa2d3e0a7279e9b480d3f36d4adea6b0fe709
hafa786 <hafizm.shehzad@gmail.com>
2019-05-17 17:34:21 +0300

    modify the style and look

--- front-end-app/src/Routes.js
+++ front-end-app/src/Routes.js
@@ -3,14 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
-import Header from "./containers/Header";
+import Page from "./containers/Page";
 import Navigation from "./containers/Navigation";
 
 export default () =>
   <Switch>
     {/* <Route path="/" exact component={Home} /> */}
     {/* <Route path="/login" exact component={Login} /> */}
-    <Route exact component={Header} />
+    <Route exact component={Page} />
     {/* <Route exact component={Navigation} /> */}
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
--- front-end-app/src/containers/Page.js
+++ front-end-app/src/containers/Page.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./Header.css";
+import "./Page.css";
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      suggestions: [],
       apiName:'Api Description',
       apiDescription:"Searches over a website or collection of websites.",
       subTopics : [],
@@ -16,6 +17,12 @@ export default class Login extends Component {
       apiRequestData: [],
       apiResponseData: [],
       apiDefinitions: [],
+      apiReadMore: true,
+      showDefinition:false,
+      definitionSchema:true,
+      definitionExample:false,
+      cseSchema: true,
+      cseExample: false,
       apiData:[
           {name:"search.cse.list", url:"https://www.googleapis.com/customsearch/v1/search.cse.list",requestData:[{
             parameterName:"quotaUser",parameterType:"String", parameterDescription:"Turns off the translation between zh-CN and zh-TW."
@@ -32,6 +39,12 @@ export default class Login extends Component {
   }
   componentDidMount() {
     console.log('Child did mount.');
+    // $("#q").autocomplete({
+    //   source: availableTags,
+    //   minLength: 0
+    //   }).focus(function(){            
+    //     $(this).autocomplete('search', $(this).val())
+    //   });
   }
   apiSelect(value){
     this.setState({apiName : value.target.value})
@@ -42,6 +55,8 @@ export default class Login extends Component {
                 {name:"2", data:"search.cse.siterestrict.list"}
             ]
         });
+        this.setState({apiReadMore:false});
+        this.setState({showDefinition:false});
     }
     else if (value.target.value == "Definitions"){
       this.setState({
@@ -53,6 +68,7 @@ export default class Login extends Component {
               {name:"7", data:"Search"}
           ]
       });
+      this.setState({apiReadMore:false});
     }
     else{
         this.setState({
@@ -64,17 +80,21 @@ export default class Login extends Component {
           this.setState({apiDescription:"Searches over a website or collection of websites"});
           this.setState({apiRequestData:[]});
           this.setState({apiResponseData:[]});
+          this.setState({apiReadMore:true});
         }
         if (value.target.value == "Definitions"){
           this.setState({apiDescription:"Subtopics"});
           this.setState({apiRequestData:[]});
           this.setState({apiResponseData:[]});
+          this.setState({apiReadMore:false});
         }
     }
     this.setState({requestAndResponseBox:false});
   }
   apiSubTopicSelect(value){
+    let definition = [ "Context" , "Promotion","Query","Result","Search"];
     console.log(value.currentTarget.dataset.id);
+    if (definition.indexOf(value.currentTarget.dataset.id) <= -1) {
     (value.currentTarget.dataset.id == "search.cse.list") ? this.setState({apiDescription:"Returns metadata about the search performed, metadata about the custom search engine used for the search, and the search results."}) :this.setState({apiDescription:"Returns metadata about the search performed, metadata about the custom search engine used for the search, and the search results. Uses a small set of url patterns."});
     this.setState({apiName:value.currentTarget.dataset.id});
     this.setState({apiMethod:"POST"});
@@ -95,6 +115,37 @@ export default class Login extends Component {
         ]
     });
     this.setState({requestAndResponseBox:true});
+    this.setState({showDefinition:false});
+  }
+  else{
+    this.setState({apiName:value.currentTarget.dataset.id});
+    this.setState({
+      apiDefinitions:[
+          {objectName:"Object",objectType:"Search",objectBody:[{data:"context:object:Context"},{data:"facets:array[array:]"}]},
+          {objectName:"Object",objectType:"Result",objectBody:[{data:"context:object:Context"},{data:"facets:array[array:]"}]},
+
+      ]
+    });
+    this.setState({showDefinition:true});
+  }
+  }
+  shiftSchemaExample(value){
+    if (value.currentTarget.dataset.id == 'Schema'){
+      this.setState({definitionSchema:true});
+      this.setState({definitionExample:false});
+    }else{
+      this.setState({definitionSchema:false});
+      this.setState({definitionExample:true});
+    }  
+  }
+  shiftResponseSchemaExample(value){
+    if (value.currentTarget.dataset.id == 'Schema'){
+      this.setState({cseSchema:true});
+      this.setState({cseExample:false});
+    }else{
+      this.setState({cseSchema:false});
+      this.setState({cseExample:true});
+    }  
   }
   render() {
     return (
@@ -129,7 +180,7 @@ export default class Login extends Component {
       <div className="container">
             <div className="content-box">
                   <div className="row">
-                  <select onChange={this.apiSelect.bind(this)} class="form-control form-control-md">
+                  <select onChange={this.apiSelect.bind(this)} class="form-control form-control-md-6">
                     <option value="Api Description">Api Description</option>
                     <option value="CSE">CSE</option>
                     <option value="Definitions">Definitions</option>
@@ -144,7 +195,14 @@ export default class Login extends Component {
             <p>{this.state.apiDescription}</p>
             </div>
           </div>
-          
+          {this.state.apiReadMore == true ?
+           <div className="row">
+            <div className="col-12 col-md-12">
+            <h2>Read More </h2>
+            <a href="https://developers.google.com/custom-search/v1/using_rest" className="custom-link">https://developers.google.com/custom-search/v1/using_rest</a>
+            </div>
+           </div>
+          :""}
           <div className="row">
             <div className="col-12 col-md-8">
                 <p>
@@ -168,7 +226,7 @@ export default class Login extends Component {
           </div>
           <div className="row">
           {this.state.requestAndResponseBox == true ? 
-            <div className="col-12 col-md-6 request-box">
+            <div className="col-12 col-md-5 request-box">
                 <h4>Request Parameters - Query - {this.state.apiName}</h4>
                 <hr className="spaces"></hr>
                 <div className="request-box-body">
@@ -176,7 +234,7 @@ export default class Login extends Component {
                 <div>
                 <span className="parameter-name">{item.parameterName}</span>
                 <span className="parameter-datatype">{item.parameterType}</span>
-                <p>{item.parameterDescription}</p><hr></hr>
+                <p className="parameter-description">{item.parameterDescription}</p><hr></hr>
                 </div>
                 )}
                 </div>
@@ -187,12 +245,14 @@ export default class Login extends Component {
             </div>
 
             {this.state.requestAndResponseBox == true ? 
-            <div className="col-12 col-md-5 response-box">
-                <h4>Response - 200</h4>
+            <div className="col-12 col-md-6 response-box">
+                <h4>Response - <button className="btn btn-success btn-xs">200</button></h4>
                 <hr></hr>
                 <div className="shift-button">
-                <button className="btn btn-outline-light">Schema</button>&nbsp;<button className="btn btn-outline-light">Example</button>
+                <button onClick={this.shiftResponseSchemaExample.bind(this)} data-id='Schema' className="btn btn-default">Schema</button>
+                <button onClick={this.shiftResponseSchemaExample.bind(this)} data-id="Example" className="btn btn-default">Example</button>
                 </div>
+                {this.state.cseSchema == true ? 
                 <div className="response-box-body">
                 { this.state.apiResponseData.map((item, key) =>
                 <div>
@@ -202,13 +262,54 @@ export default class Login extends Component {
                     <span className="parameter-name">context:</span><span className="parameter-datatype">{responseBody.data}</span><br></br>
                     </div>
                     )}
-                </div>    
+                </div>  
                 )}
                 </div>
+                : <div className="well response-box-body">{JSON.stringify(this.state.apiResponseData)}</div>
+                }
             </div>
             : '' }
+
           </div>
-          
+          {this.state.showDefinition == true ? 
+          <div className="row">
+                <div className="col-12 col-md-12">
+                </div>
+                <div className="col-12 col-md-8 definition-box">
+                <button onClick={this.shiftSchemaExample.bind(this)} data-id='Schema' className="btn btn-default">Schema</button>
+                <button onClick={this.shiftSchemaExample.bind(this)} data-id="Example" className="btn btn-default">Example</button>
+                    <h4>{this.state.apiName}</h4>
+                  {this.state.definitionSchema == true ?
+                    <div className="definition-box-body">
+                      { this.state.apiDefinitions.map((item, key) =>
+                      <div>
+                      <span className="definition-name">{item.objectName}</span>
+                      <span className="definition-datatype">{item.objectType}</span>
+                      { item.objectBody.map((responseBody, key) =>
+                      <div className="definition-object-body">
+                      <span className="definition-name">context:</span><span className="parameter-datatype">{responseBody.data}</span><br></br>
+                      </div>
+                      )}
+                      </div>
+                      )}
+                    </div>
+                  : <div>{JSON.stringify(this.state.apiDefinitions)}</div>
+                  }
+                </div>
+          </div>
+          : '' 
+          }
+        </div>
+        <br></br><br></br><br></br><br></br>
+        <div className="container">
+            <div className="row footer">
+              <div className="col-12 col-md-10">
+                <i>Copyright © 2018 LucyBot Inc.</i>
+              </div> 
+              <div className="col-12 col-md-2">
+                <a className="custom-link">Disclaimer</a>
+              </div>  
+            </div>
         </div>
       </div>
     );
